5ff2147d8d25e8e0c470d7c93cfaa2614b2606e3
Courtney & David Roberts & Eric Shou <pair+aquarian5.dfroberts2.eshou1014@devbootcamp.com>
2016-01-08 14:15:03 -0500

    Add badge model and migration

---
+++ app/models/badge.rb
@@ -0,0 +1,3 @@
+class Badge < ActiveRecord::Base
+  belongs_to :on_call_caregiver
+end
--- app/models/on_call_caregiver.rb
+++ app/models/on_call_caregiver.rb
@@ -2,6 +2,7 @@ class OnCallCaregiver < ActiveRecord::Base
   has_many :caregiver_relationships
   has_many :appointments, through: :caregiver_relationships
   has_one :login, :as => :loginable
+  has_many :badges
 
 
   validates_presence_of :education
