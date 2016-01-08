1d368052fa788528bda3e05f8a29d2e3b86fb106
David Roberts <email": null>
2016-01-08 17:16:18 -0500

    Add geocode capability to PC and OCC

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,10 +1,17 @@
 class Login < ActiveRecord::Base
   has_secure_password
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  validates :address
+  validates :address, presence: true
   validates :password, length:{minimum:6}
-  validates_confirmation_of :confirm => :password_comfirmation
+  validates_confirmation_of :password, :confirm => :password_comfirmation
   validates :zipcode, length:{maximum:5}
-  validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, presence: true
+  validates_format_of :phone, :with => /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, :on => :create
   belongs_to :loginable, :polymorphic => true
+
+  geocoded_by :city_state
+  after_validation :geocode
+
+  def city_state
+    city + ", " + state
+  end
 end
