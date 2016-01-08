5ab6b91f54a5385902fd6d9ec8d18a16a86385e2
eshou1014 <eshou1014@gmail.com>
2016-01-08 17:36:58 -0500

    Added calender, some functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
   root 'welcome#index'
+   resources :appointments
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
