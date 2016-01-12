c8f2ca01afd23b96a7290d3452748e35a80ffc21
David Roberts <email": null>
2016-01-12 12:02:23 -0500

    add text reminders that are delayed and sent according to reminder time

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'merit'
 
 #gem for running code asynchronously
 gem 'delayed_job_active_record'
+#gem for running delayed jobs
+gem "daemons"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -3,8 +3,8 @@ class RemindersController < ApplicationController
     appointment = Appointment.find_by(id: params[:appointment_id])
     reminder = Reminder.new(reminder_params)
     reminder.appointment = appointment
-    reminder.time = appointment.convert_to_datetime(Time.parse(params[:reminder]["time(5i)"]))
-    reminder.test_text(reminder.body, reminder.time) if reminder.save
+    reminder.time = appointment.convert_to_datetime(Time.parse(params[:reminder]["time(5i)"])).in_time_zone('Eastern Time (US & Canada)')+5*60*60
+    reminder.delay(run_at: reminder.time).send_text_reminder(reminder.body) if reminder.save
     redirect_to edit_primary_caregiver_appointment_path(appointment.caregiver_relationship.primary_caregiver, appointment)
   end
 
