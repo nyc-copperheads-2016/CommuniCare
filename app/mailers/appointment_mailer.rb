b266865d8f7f4991ba974d792b6210b4b09949b7
David Roberts <email": null>
2016-01-11 22:05:46 -0500

    add email confirmation to appointment scheduling events

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,12 +1,9 @@
-class Appointment < ApplicationMailer
+class AppointmentMailer < ApplicationMailer
+
   def confirm_occ(occ, pc, appointment)
      @occ = occ
      @pc = pc
      @appointment = appointment
-     @url
-     binding.pry
-    mail(to: @occ.login.email, subject: 'Your application has been accepted!') do |format|
-      format.html { render layout: 'confirm_occ' }
-    end
+     mail(from:'abc@example.com', to: occ.login.email, subject: 'Your application has been accepted!')
   end
 end
