d1331f76689e963f8da726d9e46affebef9e5872
David Roberts <email": null>
2016-01-13 13:18:11 -0500

    style profile view page

--- app/models/on_call_caregiver.rb
+++ app/models/on_call_caregiver.rb
@@ -5,7 +5,7 @@ class OnCallCaregiver < ActiveRecord::Base
   has_many :appointments, through: :caregiver_relationships
   has_one :login, :as => :loginable
   has_many :badges
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "160x160#" }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "160x160#" }, default_url: "/assets/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 
