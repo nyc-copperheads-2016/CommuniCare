b07d7644be250e902d098a826a5eabc492f31065
David Roberts <email": null>
2016-01-13 22:16:26 -0500

    style edit page

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-      :address              => "localhost",
-      :port                 => 1025,
-      :domain               => "gmail.com",
-      :user_name            => ENV["SMTP_USER"],
-      :password             => ENV["SMTP_PASSWORD"],
-      :authentication       => :plain,
-      :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#       :address              => "localhost",
+#       :port                 => 1025,
+#       :domain               => "gmail.com",
+#       :user_name            => ENV["SMTP_USER"],
+#       :password             => ENV["SMTP_PASSWORD"],
+#       :authentication       => :plain,
+#       :enable_starttls_auto => true
+# }
